50effb87fd8b056751f9098913eadfe5433f2f94
Felix-Suen <felix.suen2000@gmail.com>
2022-03-01 23:50:25 -0800

    new hats and decorations

--- nft.py
+++ nft.py
@@ -2,18 +2,20 @@ from PIL import Image
 import random
 
 count = 0
-samples = [1, 2, 3, 4]
 
 while count <= 29:
-    rand_arr = random.choices(samples, k=4)
-    background = Image.open(r'png/background/background_%s.png' % rand_arr[0])
-    poster = Image.open(r'./png/poster/poster_%s.png' % rand_arr[1])
-    balloon = Image.open(r'./png/balloon/balloon_%s.png' % rand_arr[2])
-    face = Image.open(r'./png/face/face_%s.png' % rand_arr[3])
+    background = Image.open(r'png/background/background_%s.png' % random.randint(1, 5))
+    poster = Image.open(r'./png/poster/poster_%s.png' % random.randint(1, 4))
+    balloon = Image.open(r'./png/balloon/balloon_%s.png' % random.randint(1, 4))
+    face = Image.open(r'./png/face/face_%s.png' % random.randint(1, 8))
+    decoration = Image.open(r'./png/decoration/decoration_%s.png' % random.randint(1, 3))
+    hat = Image.open(r'./png/hat/hat_%s.png' % random.randint(1, 5))
 
-    first_step = Image.alpha_composite(background, poster)
-    second_step = Image.alpha_composite(first_step, balloon)
-    final = Image.alpha_composite(second_step, face)
+    first_step = Image.alpha_composite(background, decoration)
+    second_step = Image.alpha_composite(first_step, poster)
+    third_step = Image.alpha_composite(second_step, balloon)
+    fourth_step = Image.alpha_composite(third_step, hat)
+    final = Image.alpha_composite(fourth_step, face)
 
     count += 1
     final.save('./collection/generated-%s.png' % count)
