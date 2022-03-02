2f329650d29d07ad07a079e913f11399c0a415fe
Felix-Suen <felix.suen2000@gmail.com>
2022-03-02 00:29:16 -0800

    gif maker

--- nft.py
+++ nft.py
@@ -1,14 +1,15 @@
 from PIL import Image
 import random
+import imageio
+import os
 
 count = 0
-
 while count <= 29:
     background = Image.open(r'png/background/background_%s.png' % random.randint(1, 5))
     poster = Image.open(r'./png/poster/poster_%s.png' % random.randint(1, 4))
     balloon = Image.open(r'./png/balloon/balloon_%s.png' % random.randint(1, 4))
     face = Image.open(r'./png/face/face_%s.png' % random.randint(1, 8))
-    decoration = Image.open(r'./png/decoration/decoration_%s.png' % random.randint(1, 3))
+    decoration = Image.open(r'./png/decoration/decoration_%s.png' % random.randint(1, 4))
     hat = Image.open(r'./png/hat/hat_%s.png' % random.randint(1, 5))
 
     first_step = Image.alpha_composite(background, decoration)
@@ -19,3 +20,10 @@ while count <= 29:
 
     count += 1
     final.save('./collection/generated-%s.png' % count)
+
+images = []
+generated_dir = './collection'
+for filename in sorted(os.listdir(generated_dir)):
+    file_path = os.path.join(generated_dir, filename)
+    images.append(imageio.imread(file_path))
+imageio.mimsave('./collection/collection.gif', images, duration=0.3)
