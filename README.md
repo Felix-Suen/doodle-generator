0457e55ac6e348b9efba6c9409fcd06507c8f126
Felix-Suen <felix.suen2000@gmail.com>
2022-03-10 00:49:18 -0800

    improved links

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python Script to Generate my own NFT
+Try it here [demo](https://share.streamlit.io/felix-suen/gen-nft/site.py)
 ![Alt Text](https://github.com/Felix-Suen/gen-nft/blob/master/collection.gif)
--- site.py
+++ site.py
@@ -3,31 +3,40 @@ import random
 import streamlit as st
 import base64
 
-st.markdown("# Generate a random NFT or avatar")
+st.title("Generate your own avatar :dog:")
 
-# encode the image
+# enable image download
 def imagedownload(filename):
     image_file = open(filename, 'rb')
     b64 = base64.b64encode(image_file.read()).decode()  # strings <-> bytes conversions
     href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download Image</a>'
     return href
 
-if st.button('Random generate'):
-    background = Image.open(r'png/background/background_%s.png' % random.randint(1, 5))
-    poster = Image.open(r'./png/poster/poster_%s.png' % random.randint(1, 4))
-    balloon = Image.open(r'./png/balloon/balloon_%s.png' % random.randint(1, 4))
-    face = Image.open(r'./png/face/face_%s.png' % random.randint(1, 8))
-    decoration = Image.open(r'./png/decoration/decoration_%s.png' % random.randint(1, 4))
-    hat = Image.open(r'./png/hat/hat_%s.png' % random.randint(1, 5))
+project_names = ['Crazy Balloon', 'Tennis Ball']
+project = st.radio("Project Select", project_names)
 
-    first_step = Image.alpha_composite(background, decoration)
-    second_step = Image.alpha_composite(first_step, poster)
-    third_step = Image.alpha_composite(second_step, balloon)
-    fourth_step = Image.alpha_composite(third_step, hat)
-    final = Image.alpha_composite(fourth_step, face)
+if project == 'Crazy Balloon':
+    st.subheader('Crazy Balloon')
+    if st.button('Generate Image'):
+        background = Image.open(r'png/crazy-balloon/background/background_%s.png' % random.randint(1, 5))
+        poster = Image.open(r'./png/crazy-balloon/poster/poster_%s.png' % random.randint(1, 4))
+        balloon = Image.open(r'./png/crazy-balloon/balloon/balloon_%s.png' % random.randint(1, 4))
+        face = Image.open(r'./png/crazy-balloon/face/face_%s.png' % random.randint(1, 8))
+        decoration = Image.open(r'./png/crazy-balloon/decoration/decoration_%s.png' % random.randint(1, 4))
+        hat = Image.open(r'./png/crazy-balloon/hat/hat_%s.png' % random.randint(1, 5))
 
-    final.save('./random.png')
+        first_step = Image.alpha_composite(background, decoration)
+        second_step = Image.alpha_composite(first_step, poster)
+        third_step = Image.alpha_composite(second_step, balloon)
+        fourth_step = Image.alpha_composite(third_step, hat)
+        final = Image.alpha_composite(fourth_step, face)
 
-st.image(Image.open("./random.png"))
-st.markdown(imagedownload('./random.png'), unsafe_allow_html=True)
+        final.save('./random.png')
+    st.image(Image.open("./random.png"))
+    st.markdown(imagedownload('./random.png'), unsafe_allow_html=True)
+
+if project == 'Tennis Ball':
+    st.subheader(project)
+
+
 
