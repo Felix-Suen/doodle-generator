8dfb3159f285fb4ea5cca9dc48f8229f1054b274
Felix-Suen <felix.suen2000@gmail.com>
2022-03-24 14:44:14 -0700

    foxie

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,39 @@ def imagedownload(filename):
     href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download Image</a>'
     return href
 
-project_names = ['Tennis Ball', 'Crazy Balloon']
+project_names = ['Foxiez', 'Tennis Ball', 'Crazy Balloon']
 project = st.radio("Project Select", project_names)
+
+if project == 'Foxiez':
+    st.subheader(project)
+
+    list_background = ['rice', 'pink', 'yellow', 'green', 'blue-dots', 'green-dots', 'yellow-dots', 'snow']
+    list_expressions = ['default-smile', 'eyes-closed', 'frown', 'kawaii-smile', 'sad', 'sleepy', 'sophisticated', 'wink']
+    list_hats = ['none', 'brim-bucket', 'bucket', 'mushroom', 'strawhat']
+    list_body = ['body']
+
+    col1, col2 = st.columns(2)
+    option_expressions = col1.selectbox('Expressions', list_expressions)
+    option_hats = col1.selectbox('Hats', list_hats)
+    option_background = col1.selectbox('Background', list_background)
+
+    if col2.button('Randomize'):
+        option_expressions = list_expressions[random.randint(0, len(list_expressions)-1)]
+        option_hats = list_hats[random.randint(0, len(list_hats)-1)]
+        option_background = list_background[random.randint(0, len(list_background)-1)]
+
+    background = Image.open(r'png/foxie/background/%s.png' % option_background)
+    body = Image.open(r'png/foxie/body.png')
+    hat = Image.open(r'png/foxie/hats/%s.png' % option_hats)
+    expression = Image.open(r'png/foxie/expressions/%s.png' % option_expressions)
+
+    first_step = Image.alpha_composite(background, body)
+    second_step = Image.alpha_composite(first_step, hat)
+    final = Image.alpha_composite(second_step, expression)
+
+    final.save('./foxie.png')
+    col2.image(Image.open("./foxie.png"))
+    col2.write(imagedownload('./foxie.png'), unsafe_allow_html=True)
 
 if project == 'Tennis Ball':
     st.subheader(project)
